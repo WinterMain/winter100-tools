df525da2cb1d623fd6ab31c6a91504cdfaf798bc
Winter Yang <winter.yang@vinotech.com>
2024-09-22 22:17:03 +0800

    Relase：20240922221703

---
+++ _nuxt/Q9FHTszc.js
@@ -0,0 +1,1 @@
+import{Q as o}from"./pPK1IDfl.js";import{o as t,c as a,b as c}from"./cZh0Q0wy.js";const r={class:"c-common-qrcode"},i={__name:"Qrcode",props:{value:{type:String,default:""},size:{type:Number,default:124}},setup(e){return(s,l)=>(t(),a("div",r,[c(o,{value:e.value,size:e.size},null,8,["value","size"])]))}};export{i as _};
